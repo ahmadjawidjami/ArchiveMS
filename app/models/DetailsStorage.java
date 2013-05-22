0e22f2db2aee23771f06369a676f9b85849f0b92
ahmadjawidjami <ahmadjawidjami@gmail.com>
2013-05-22 14:38:54 +0430

    some structures added

---
+++ app/models/DetailsStorage.java
@@ -0,0 +1,13 @@
+package models;
+
+import java.util.Date;
+
+public class DetailsStorage {
+	public int id;
+	public String name;
+	public String category;
+	public Date publish;
+	public Date record;
+	public Date archive;
+
+}
