d95df0aede41bbf7e56acdc9c29d6d2d638d6dfc
Aalem <Aalem.daneshyar87@gmail.com>
2013-05-23 22:13:30 +0430

    Delete category update

--- app/controllers/CategoryRemover.java
+++ app/controllers/CategoryRemover.java
@@ -1,8 +1,35 @@
 package controllers;
 
-public class CategoryRemover {
-	
-	
-	
+import models.CategoryStorage;
+import play.data.Form;
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class CategoryRemover extends Controller {
+	static String categoryName;
+	static CategoryStorage categoryRem;
+	static Form<CategoryStorage> categoryRemove;
+
+	public static Result removeCategory() {
+		categoryRemove = Form.form(CategoryStorage.class).bindFromRequest();
+		categoryRem = categoryRemove.get();
+		if (categoryRem.categoryName != null) {
+			removeCategoryFromDB();
+			removeCategoryFromDisk();
+			categoryName = categoryRem.categoryName;
+			return ok("Category '" + categoryName + "' is now Deleted");
+		} else
+			return notFound("No category was selected");
+	}
+
+	public static void removeCategoryFromDB() {
+		categoryRem.delete();
+
+	}
+
+	public static void removeCategoryFromDisk() {
+		
+		
+	}
 
 }
--- app/models/CategoryStorage.java
+++ app/models/CategoryStorage.java
@@ -1,13 +1,18 @@
+
 package models;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 import play.db.ebean.Model;
+import scala.collection.immutable.List;
 @Entity
 public class CategoryStorage extends Model {
 	
 	
-	@id
+	@Id
 	public String categoryName;
+	public static Finder<String, CategoryStorage> find = new Finder<String, CategoryStorage>(
+			String.class, CategoryStorage.class);
 
 }
