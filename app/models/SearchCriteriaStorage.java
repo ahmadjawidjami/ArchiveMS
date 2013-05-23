18695096d339ed7736a70da8257f9553a9ced984
ahmadjawidjami <ahmadjawidjami@gmail.com>
2013-05-24 01:40:15 +0430

    me

---
+++ app/models/SearchCriteriaStorage.java
@@ -0,0 +1,15 @@
+package models;
+
+import java.util.Date;
+
+public class SearchCriteriaStorage {
+	
+	public String name;
+	public String tag;
+	public String category;
+	public Date recordDate;
+	public Date publishDate;
+	public Date archiveDate;
+	
+
+}
