574c4af01dc4059ff831f072b84e3fcf74cdd8ec
Fardin <fardinhakimi@gmail.com>
2013-06-18 17:24:05 +0430

    authentication almost done

--- app/models/Key.java
+++ app/models/Key.java
@@ -4,5 +4,12 @@ public class Key {
 	
 	public String username;
 	public String password;
+	
+	public String validate() {
+        if(UserStorage.authenticate(username, password) == null) {
+            return "Invalid user or password";
+        }
+        return null;
+    }
 
 }
